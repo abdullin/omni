3a0f7e0e069c1c7e4829afe48bdce2c78fcfcbfe
Rinat Abdullin <rinat.abdullin@gmail.com>
2015-01-20 14:48:11 +0500

    all: strip from the domain

--- back/reports/module.go
+++ back/reports/module.go
@@ -1,37 +1,12 @@
 package reports
 
-import (
-	"bitbucket.org/abdullin/proto/back/api"
-	"bitbucket.org/abdullin/proto/back/events"
-	"bitbucket.org/abdullin/proto/back/module"
-)
+import "bitbucket.org/abdullin/proto/back/module"
 
 type Module struct {
 	pub module.Publisher
 }
 
 func (m *Module) Register(r module.Registrar) {
-	r.HandleHttp("GET", "/reports/groups", m.listGroups)
-
-	var s = &store{}
-	var d1 = &feedDenormalizer{s}
-
-	r.HandleEvents("feed", d1)
-}
-
-func (m *Module) listGroups(r *api.Request) api.Response {
-	// wild response
-	//m.pub.MustPublish(&events.ProductCreated{})
-
-	return api.NewJSON(&GroupReportPageDto{
-		Items: []*GroupReportItem{
-			&GroupReportItem{
-				Quantity: 3,
-				Name:     "hardcoded",
-				GroupId:  events.NewProductId(),
-			},
-		},
-	})
 }
 
 func NewModule(pub module.Publisher) module.Module {
@@ -39,7 +14,7 @@ func NewModule(pub module.Publisher) module.Module {
 }
 
 var Spec = &module.Spec{
-	Name:     "inbox",
+	Name:     "reports",
 	Factory:  NewModule,
 	UseCases: cases,
 }
