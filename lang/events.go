10ac25dc50ebdd4a9808431b22330362d3e18adb
Rinat Abdullin <rinat.abdullin@gmail.com>
2015-01-21 16:47:18 +0500

    lang: moved events to a separate file

---
+++ lang/events.go
@@ -0,0 +1,59 @@
+package lang
+
+import . "github.com/abdullin/omni/core"
+
+func i(contract string, eventId EventId) *Info {
+	return NewInfo(contract, eventId.Id)
+}
+
+type TaskAdded struct {
+	EventId EventId `json:"eventId"`
+	TaskId  TaskId  `json:"taskId"`
+	Name    string  `json:"name"`
+	Inbox   bool    `json:"inbox"`
+}
+
+func NewTaskAdded(event EventId, task TaskId, name string, inbox bool) *TaskAdded {
+	return &TaskAdded{event, task, name, inbox}
+}
+
+type TaskChecked struct {
+	EventId EventId `json:"eventId"`
+	TaskId  TaskId  `json:"taskId"`
+}
+
+func NewTaskChecked(event EventId, task TaskId) *TaskChecked {
+	return &TaskChecked{event, task}
+}
+
+type TaskUnchecked struct {
+	EventId EventId `json:"eventId"`
+	TaskId  TaskId  `json:"taskId"`
+}
+
+func NewTaskUnhecked(event EventId, task TaskId) *TaskUnchecked {
+	return &TaskUnchecked{event, task}
+}
+
+type TaskStarred struct {
+	EventId EventId `json:"eventId"`
+	TaskId  TaskId  `json:"taskId"`
+}
+
+func NewTaskStarred(event EventId, task TaskId) *TaskStarred {
+	return &TaskStarred{event, task}
+}
+
+type TaskUnstarred struct {
+	EventId EventId `json:"eventId"`
+	TaskId  TaskId  `json:"taskId"`
+}
+
+func NewTaskUnstarred(event EventId, task TaskId) *TaskUnstarred {
+	return &TaskUnstarred{event, task}
+}
+
+func (e *TaskAdded) Meta() *Info     { return i("TaskAdded", e.EventId) }
+func (e *TaskChecked) Meta() *Info   { return i("TaskChecked", e.EventId) }
+func (e *TaskStarred) Meta() *Info   { return i("TaskStarred", e.EventId) }
+func (e *TaskUnstarred) Meta() *Info { return i("TaskUnstarred", e.EventId) }
